17da9969ace426b982562010a4309edfd34e2c0e
timmdkrtf <contact@mdkrtf.co.id>
2025-04-07 09:02:24 +0700

    unpublish

--- src/components/AboutPT.jsx
+++ src/components/AboutPT.jsx
@@ -5,9 +5,9 @@ export default function AboutPT(){
                 <div className="content">
                     <h2>Tentang Kami</h2>
                     <div className="text">
-                        <p>PT Abdi Saji Kuliner didirikan pada tahun 2024. Kami adalah perusahaan yang bergerak di bidang food and beverages yang merupakan anak perusahaan dari Thamrin Meyer Corporation.</p>
-                        <p>Kami berkomitmen untuk menyediakan kuliner yang mengutamakan kehalalan dan thoyyib menciptakan pengalaman makan yang tidak hanya lezat, tetapi juga sehat dan berkualitas.</p>
-                        <p>Kami hadir sebagai wadah yang terus berinovasi dan mempertahankan komitmen terhadap kehalalan serta thoyyib terhadap kuliner untuk membangun loyalitas konsumen.</p>
+                        <p>PT Abdi Saji Kuliner didirikan pada tahun 2024. Kami adalah perusahaan yang bergerak di bidang <span className="fst-italic">food and beverages</span> yang merupakan anak perusahaan dari Thamrin Meyer Corporation.</p>
+                        <p>Kami berkomitmen untuk menyediakan kuliner yang mengutamakan kehalalan dan <span className="fst-italic">thoyyib</span> menciptakan pengalaman makan yang tidak hanya lezat, tetapi juga sehat dan berkualitas.</p>
+                        <p>Kami hadir sebagai wadah yang terus berinovasi dan mempertahankan komitmen terhadap kehalalan serta <span className="fst-italic">thoyyib</span> terhadap kuliner untuk membangun loyalitas konsumen.</p>
                     </div>
                 </div>
             </div>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,28 +1,28 @@
-import Navbar from "../components/Navbar"
-import Cover from "../components/Cover"
-import AboutPT from "../components/AboutPT"
-import AboutUs from "../components/AboutUs"
-import Brand from "../components/Brand"
-import Footer from "../components/Footer"
+// import Navbar from "../components/Navbar"
+// import Cover from "../components/Cover"
+// import AboutPT from "../components/AboutPT"
+// import AboutUs from "../components/AboutUs"
+// import Brand from "../components/Brand"
+// import Footer from "../components/Footer"
 
 import ASKLogo from "../assets/image/logo/ask-main-p.png"
 
 export default function Landing(){
     return(
         <>
-            <Navbar />
-            <Cover />
-            <AboutPT />
-            <AboutUs />
+            {/* <Navbar /> */}
+            {/* <Cover /> */}
+            {/* <AboutPT /> */}
+            {/* <AboutUs /> */}
             {/* <Brand /> */}
-            <Footer />
+            {/* <Footer /> */}
 
-            {/* <div className="landing">
+            <div className="landing">
                 <div className="content">
                     <img src={ASKLogo} alt="" />
                     <h3>Under Maintenance</h3>
                 </div>
-            </div> */}
+            </div>
         </>
     );
 }
